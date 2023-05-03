a486f0627c586c4289ccbf77306250e37caecb3e
unknown <partharora557@gmail.com>
2023-05-03 20:30:13 +0530

    donne

--- src/views/index.jsx
+++ src/views/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return <div className="w-[100%] h-[100vh] bg-[#fff]  overflow-hidden"></div>;
+  return <div className="w-[100%] h-[100vh] bg-[#000]  overflow-hidden"></div>;
 };
 
 export default Home;
