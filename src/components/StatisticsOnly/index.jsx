d932d87d7b56028dcd20c4d32ca833db171af2ea
unknown <partharora557@gmail.com>
2023-05-04 15:05:01 +0530

    done

--- src/components/StatisticsOnly/index.jsx
+++ src/components/StatisticsOnly/index.jsx
@@ -8,7 +8,7 @@ import useHeight from "../../hooks/useHeigth";
 const StatisticsOnlyContainer = () => {
   return (
     <>
-      <div className="w-[100%] h-[100vh] relative overflow-x-hidden overflow-y-scroll scroll-bar-cool flex-col items-center">
+      <div className="w-[100%] h-[100vh] relative overflow-x-hidden overflow-y-scroll flex scroll-bar-cool flex-col items-center">
         <Close />
 
         {/* <GlobeConnectionsExample
@@ -20,33 +20,52 @@ const StatisticsOnlyContainer = () => {
           className="w-[100%] h-[100vh] "
           frameborder="0"
         ></iframe> */}
+        <h1 className="text-center text-[#fff] text-[28px] mb-[50px] mt-[100px] font-semibold">
+          Marvelous Reports
+        </h1>
         <iframe
           src="https://flo.uri.sh/visualisation/13639521/embed"
           title="Interactive or visual content"
           class="flourish-embed-iframe"
           frameborder="0"
           scrolling="no"
-          className="w-[100%] h-[600px]"
+          className="w-[80%] h-[600px] bg-[transparent] shrink-0 mb-[100px]"
           sandbox="allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation"
         ></iframe>
+        <h1 className="text-center text-[#fff] text-[28px] mb-[50px]  font-semibold">
+          We will Take your business to the sky!
+        </h1>
         <iframe
           src="https://flo.uri.sh/visualisation/13639531/embed"
           title="Interactive or visual content"
           class="flourish-embed-iframe"
           frameborder="0"
           scrolling="no"
-          className="w-[100%] h-[600px]"
+          className="w-[80%] h-[600px] bg-[transparent] shrink-0 mb-[100px]"
           sandbox="allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation"
         ></iframe>
+        <h1 className="text-center text-[#fff] tracking-wide text-[28px] mb-[50px]  font-semibold">
+          Brilliants Representations
+        </h1>
         <iframe
           src="https://flo.uri.sh/visualisation/13639491/embed"
           title="Interactive or visual content"
           class="flourish-embed-iframe"
           frameborder="0"
           scrolling="no"
-          className="w-[100%] h-[600px]"
+          className="w-[80%] h-[600px] bg-[transparent] shrink-0 mb-[100px]"
           sandbox="allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation"
         ></iframe>
+        {/* <iframe
+          src="https://flo.uri.sh/visualisation/13639521/embed"
+          title="Interactive or visual content"
+          class="flourish-embed-iframe"
+          frameborder="0"
+          scrolling="no"
+          className="w-[100%] h-[600px]"
+
+          sandbox="allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation"
+        ></iframe> */}
         {/* <D3Container/> */}
       </div>
     </>
