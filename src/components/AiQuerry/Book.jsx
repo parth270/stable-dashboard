a0b002ac5056ba50d93920200fcbabaa6e7da294
unknown <partharora557@gmail.com>
2023-05-30 01:17:40 +0530

    done

--- src/components/AiQuerry/Book.jsx
+++ src/components/AiQuerry/Book.jsx
@@ -85,8 +85,8 @@ const FlipBook = ({ files }) => {
       console.log(x, check.width);
       console.log(y, check.height);
       if (check.width > 1400) {
-        x = ((check.height - 220) * 756) / 538;
-        y = check.height - 220;
+        x = ((check.height - 200) * 756) / 538;
+        y = check.height - 200;
       }
       // 1404,904
       if (x > check.width) {
