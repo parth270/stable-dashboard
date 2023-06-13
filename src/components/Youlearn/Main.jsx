d727a689596e52923238a08133537dcfce097521
unknown <partharora557@gmail.com>
2023-06-13 12:43:12 +0530

    done

--- src/components/Youlearn/Main.jsx
+++ src/components/Youlearn/Main.jsx
@@ -275,7 +275,7 @@ const Mcqs = ({ data, curr, next, prev }) => {
 
 const Content = ({ data }) => {
   const opts = {
-    height: "550",
+    height: "400",
     width: "100%",
   };
   const url = data.youtube_link;
@@ -306,57 +306,60 @@ const Content = ({ data }) => {
       ease={Power4.easeInOut}
     >
       <div className="w-[100%] overflow-y-auto h-[100vh] px-[100px] scroll-bar-cool">
-        <h1 className="w-[100%] text-[50px] mb-[30px] text-[#fff] tracking-wide font-bold fckin pt-[70px]  text-center">
-          Title:Youtube Video Title
+        <h1 className="w-[100%] text-[40px] mb-[30px] text-[#fff] tracking-wide font-bold fckin pt-[70px]  text-center">
+          {data.title}
         </h1>
-        <div className="w-[100%] h-[550px] relative">
-          <div className="w-[100%] flex items-center justify-center h-[100%] bg-[#00000060] backdrop-blur-xl rounded-sm absolute z-10">
-            <svg
-              aria-hidden="true"
-              className="w-5 mr-2 h-5 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
-              viewBox="0 0 100 101"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                fill="#ccc"
-              />
-              <path
-                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                fill={"#e85b40"}
-              />
-            </svg>
-          </div>
-
-          <div className="w-[100%] h-[100%] backdrop-blur-xl rounded-sm absolute z-20">
-            <YouTube opts={opts} videoId={videoId} />
-          </div>
-        </div>
-        <div
-          className=" bg-[#00000060] mt-[40px] px-[50px] rounded-[3px]
-       pt-[30px] pb-[30px] backdrop-blur-xl w-[100%]"
-        >
-          <p className="text-[25px] text-[#fff] mb-[10px] tracking-wide fckin font-medium ">
-            Summary:
-          </p>
-          <p className="text-[15px] text-[#fff] tracking-wide fckin font-medium ">
-            {data.summary}
-          </p>
-          <p className="text-[25px] mt-[30px] text-[#fff] mb-[10px] tracking-wide fckin font-medium ">
-            Notes:
-          </p>
-          {data.important_points.map((item, i) => {
-            return (
-              <p
-                className="text-[15px] flex mb-[5px] text-[#fff] tracking-wide fckin font-medium "
-                key={i}
+        <div className="flex justify-between" >
+          <div className="w-[48%] h-[400px] relative">
+            <div className="w-[100%] flex items-center justify-center h-[100%] bg-[#00000060] backdrop-blur-xl rounded-sm absolute z-10">
+              <svg
+                aria-hidden="true"
+                className="w-5 mr-2 h-5 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
               >
-                <span className="mr-[5px]">•</span> <span>{item}</span>
-              </p>
-            );
-          })}
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="#ccc"
+                />
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill={"#e85b40"}
+                />
+              </svg>
+            </div>
+
+            <div className="w-[100%] h-[100%] backdrop-blur-xl rounded-sm absolute z-20">
+              <YouTube opts={opts} videoId={videoId} />
+            </div>
+          </div>
+          <div
+            className=" bg-[#00000060] px-[50px] rounded-[3px]
+       pt-[30px] pb-[30px] backdrop-blur-xl w-[48%] h-[400px] overflow-y-auto"
+          >
+            <p className="text-[25px] text-[#fff] mb-[10px] tracking-wide fckin font-medium ">
+              Summary:
+            </p>
+            <p className="text-[15px] text-[#fff] tracking-wide fckin font-medium ">
+              {data.summary}
+            </p>
+            <p className="text-[25px] mt-[30px] text-[#fff] mb-[10px] tracking-wide fckin font-medium ">
+              Notes:
+            </p>
+            {data.important_points.map((item, i) => {
+              return (
+                <p
+                  className="text-[15px] flex mb-[5px] text-[#fff] tracking-wide fckin font-medium "
+                  key={i}
+                >
+                  <span className="mr-[5px]">•</span> <span>{item}</span>
+                </p>
+              );
+            })}
+          </div>
         </div>
+
         {curr === 0 && (
           <Mcqs
             data={data}
